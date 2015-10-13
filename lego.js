8ee98c490d050620ef54b256ccb9012eff5ac609
Sergey Gogolev <gogoleff@yandex-team.ru>
2015-10-13 19:11:10 +0500

    Добавлена задача

---
+++ lego.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// Метод, который будет выполнять операции над коллекцией один за другим
+module.exports.query = function (collection /* операторы через запятую */) {
+
+};
+
+// Оператор reverse, который переворачивает коллекцию
+module.exports.reverse = function () {
+    return function (collection) {
+        var changedCollection = collection.reverse();
+
+        // Возращаем изменённую коллекцию
+        return changedCollection;
+    };
+};
+
+// Оператор limit, который выбирает первые N записей
+module.exports.limit = function (n) {
+    // Магия
+};
+
+// Вам необходимо реализовать остальные операторы:
+// select, filterIn, filterEqual, sortBy, format, limit
+
+// Будет круто, если реализуете операторы:
+// or и and
