59feb8395628800db4ae2346bb84e46a71bdaac3
NekrasovAlexey <creepy404nf@gmail.com>
2015-10-15 21:56:26 +0500

    Update lego.js

--- lego.js
+++ lego.js
@@ -1,27 +1,106 @@
 'use strict';
 
-// Метод, который будет выполнять операции над коллекцией один за другим
-module.exports.query = function (collection /* операторы через запятую */) {
-
+module.exports.query = function (collection) {
+    var validContact = clone(collection);
+    var len = arguments.length;
+    for (var i = 1; i < len; i++) {
+        arguments[i](validContact);
+    }
+    return validContact;
 };
 
-// Оператор reverse, который переворачивает коллекцию
+function clone(obj) {
+    if (null == obj || "object" != typeof obj) return obj;
+    var copy = obj.constructor();
+    for (var attr in obj) {
+        if (obj.hasOwnProperty(attr)) copy[attr] = obj[attr];
+    }
+    return copy;
+}
+
 module.exports.reverse = function () {
     return function (collection) {
-        var changedCollection = collection.reverse();
-
-        // Возращаем изменённую коллекцию
-        return changedCollection;
+        return collection.reverse();
     };
 };
 
-// Оператор limit, который выбирает первые N записей
 module.exports.limit = function (n) {
-    // Магия
+    return function (collection) {
+        collection.splice(n);
+        return collection;
+    };
 };
 
-// Вам необходимо реализовать остальные операторы:
-// select, filterIn, filterEqual, sortBy, format, limit
+module.exports.select = function () {
+    var args = [].slice.call(arguments);
+    return function (collection) {
+        var countCont = collection.length;
+        for (var i = 0; i < countCont; i++) {
+            var contact = collection[i];
+            var fields = Object.keys(contact);
+            var countFields = fields.length;
+            for (var j = 0; j < countFields; j++) {
+                var field = fields[j];
+                if (args.indexOf(field) < 0) {
+                    delete contact[field];
+                }
+            }
+        }
+    };
+};
 
-// Будет круто, если реализуете операторы:
-// or и and
+module.exports.filterIn = function (field, filter) {
+    return function (collection) {
+        var len = collection.length;
+        var offset = 0;
+        for (var i = 0; i < len; i++) {
+            var contact = collection[i-offset];
+            var countFilter = filter.length;
+            var isValid = false;
+            for (var j = 0; j < countFilter; j++) {
+                if (contact[field] === filter[j]) {
+                    isValid = true;
+                    break;
+                }
+            }
+            if (!isValid) {
+                collection.splice(i - offset, 1);
+                offset++;
+            }
+        }
+    }
+};
+
+module.exports.sortBy = function (field, sort) {
+    return function (collection) {
+        collection.sort(function (a, b) {
+            var aField = a[field];
+            var bField = b[field];
+            var compareRes;
+            if (Number.isInteger(aField) && Number.isInteger(bField)) {
+                compareRes = aField - bField;
+            } else {
+                compareRes = aField.localeCompare(bField, 'en', {numeric: true});
+            }
+            if (sort == 'asc') {
+                return compareRes;
+            } else {
+                return -compareRes;
+            }
+        })
+    }
+};
+
+module.exports.format = function (field, func) {
+    return function (collection) {
+        var len = collection.length;
+        for (var i=0; i<len; i++) {
+            var contact = collection[i];
+            contact[field] = func(contact[field]);
+        }
+    }
+};
+
+module.exports.filterEqual = function (field, value) {
+    return this.filterIn(field, [value]);
+};
