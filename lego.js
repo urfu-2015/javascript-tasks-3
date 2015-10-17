7cb54258666e987f0ad484f7e2d37d39cca81f7c
sorovlad <fafafa@e1.ru>
2015-10-18 01:04:20 +0500

    Готово

--- lego.js
+++ lego.js
@@ -1,8 +1,50 @@
 'use strict';
 
 // Метод, который будет выполнять операции над коллекцией один за другим
-module.exports.query = function (collection /* операторы через запятую */) {
 
+module.exports.query = function (collection) {
+    if (typeof collection !== 'object') {
+        return null;
+    }
+    var legoCollection = collection.map(function (item) {
+        return Object.assign({}, item);;
+    });
+
+
+    for (var i = 1; i < arguments.length; i ++) {
+        legoCollection = arguments[i](legoCollection);
+    }
+    return legoCollection;
+};
+
+module.exports.select = function () {
+    var fields = [].slice.call(arguments);
+    return function (collection) {
+        return collection.map(function (item) {
+            for (var key in item) {
+                if (fields.indexOf(key) === -1) {
+                    delete item[key];
+                }
+            }
+            return item;
+        });
+    };
+};
+
+module.exports.filterIn = function (field, elements) {
+    return function (collection) {
+        return collection.filter(function (item) {
+            return elements.indexOf(item[field]) !== -1;
+        });
+    };
+};
+
+module.exports.filterEqual = function (field, element) {
+    return function (collection) {
+        return collection.filter(function (item) {
+            return item[field] === element;
+        });
+    };
 };
 
 // Оператор reverse, который переворачивает коллекцию
@@ -15,13 +57,43 @@ module.exports.reverse = function () {
     };
 };
 
-// Оператор limit, который выбирает первые N записей
-module.exports.limit = function (n) {
-    // Магия
+module.exports.sortBy = function (field, type) {
+    return function (collection) {
+        if (type === 'asc') {
+            return collection.sort(function (a, b) {
+                return a[field] >= b[field] ? 1 : -1;
+            });
+        } else {
+            return collection.sort(function (a, b) {
+                return b[field] >= a[field] ? 1 : -1;
+            });
+        }
+    };
 };
 
-// Вам необходимо реализовать остальные операторы:
-// select, filterIn, filterEqual, sortBy, format, limit
+module.exports.format = function (field, filter) {
+    return function (collection) {
+        return collection.map(function (item) {
+            item[field] = filter(item[field]);
+            return item;
+        });
+    };
+};
 
-// Будет круто, если реализуете операторы:
-// or и and
+// Оператор limit, который выбирает первые N записей
+module.exports.limit = function (n) {
+    return function (collection) {
+        return collection.slice(0, n);
+    };
+};
+
+module.exports.or = function (conditionFirst, conditionSecond) {
+    return function (collection) {
+        return conditionFirst(collection).concat(conditionSecond(collection));
+    };
+};
+module.exports.and = function (conditionFirst, conditionSecond) {
+    return function (collection) {
+        return conditionFirst(conditionSecond(collection));
+    };
+};
