3a1108baa91e85d292df197a4160a7a0d8fe7a6f
Mironov Alexey <mironov.alexey.fiit.urfu@gmail.com>
2015-10-18 23:46:26 +0500

    query, select, filterIn, filterEqual, sortBy, format и limit без допов

--- lego.js
+++ lego.js
@@ -1,25 +1,86 @@
 'use strict';
 
 // Метод, который будет выполнять операции над коллекцией один за другим
-module.exports.query = function (collection /* операторы через запятую */) {
-
+module.exports.query = function (collection) {
+    var operators = Object.keys(arguments).map(key => arguments[key]).slice(1);
+    return operators.reduce(function(currentCollection, operator) {
+        return operator(currentCollection);
+    },
+    collection.slice());
 };
 
 // Оператор reverse, который переворачивает коллекцию
 module.exports.reverse = function () {
-    return function (collection) {
-        var changedCollection = collection.reverse();
-
-        // Возращаем изменённую коллекцию
-        return changedCollection;
+    return collection => {
+        return collection.slice().reverse();
     };
 };
 
 // Оператор limit, который выбирает первые N записей
 module.exports.limit = function (n) {
-    // Магия
+    return collection => {
+        if (isNaN(n) || n < 0) {
+            throw new RangeError('n должно быть числом и >= 0')
+        }
+        return collection.slice(0, n);
+    };
 };
 
+module.exports.select = function() {
+    var fields = Object.keys(arguments).map(key => arguments[key]);
+    return collection => {
+        return collection.map(contact => {
+            var result = {};
+            fields.forEach(field => {
+                if (field in contact) {
+                        result[field] = contact[field];
+                    }
+            });
+            return result;
+        });
+    };
+};
+
+module.exports.filterIn = function(field, values) {
+    return collection => {
+        return collection.filter(contact => {
+                return values.some(value => value === contact[field]);
+            });
+    };
+};
+
+module.exports.filterEqual = function(field, value) {
+    return collection => {
+        return collection.filter(contact => value === contact[field]);
+    };
+};
+
+module.exports.sortBy = function(field, order) {
+    if (!(order === 'asc' || order === 'desc')) {
+        throw new TypeError('Порядок сортировки должен быть asc или desc');
+    }
+    var compareContact = function(contact1, contact2) {
+        if (contact1[field] > contact2[field]) {
+            return 1;
+        }
+        return -1;
+    };
+    return function(collection) {
+        if (order === 'asc') {
+            return collection.sort(compareContact);
+        }
+        return collection.sort(compareContact).reverse();
+    }
+}
+
+module.exports.format = function(field, formatter) {
+    return collection => {
+        collection.forEach(contact => {
+            contact[field] = formatter(contact[field]);
+        });
+        return collection;
+    };
+}
 // Вам необходимо реализовать остальные операторы:
 // select, filterIn, filterEqual, sortBy, format, limit
 
