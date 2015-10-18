37b82dd6a2cf77fdacf1db1a50603de19f09d8b5
NikShel <nikshelekb@yandex.ru>
2015-10-18 19:57:56 +0500

    Made task

--- lego.js
+++ lego.js
@@ -2,7 +2,10 @@
 
 // Метод, который будет выполнять операции над коллекцией один за другим
 module.exports.query = function (collection /* операторы через запятую */) {
-
+    for (var i = 1; i < arguments.length; i++) {
+        collection = arguments[i](collection);
+    }
+    return collection;
 };
 
 // Оператор reverse, который переворачивает коллекцию
@@ -17,11 +20,83 @@ module.exports.reverse = function () {
 
 // Оператор limit, который выбирает первые N записей
 module.exports.limit = function (n) {
-    // Магия
+    return function (collection) {
+        var newCollection = [];
+        for (var i = 0; i < n; i++) {
+            newCollection.push(collection[i]);
+        }
+        return newCollection;
+    };
 };
 
-// Вам необходимо реализовать остальные операторы:
-// select, filterIn, filterEqual, sortBy, format, limit
+module.exports.select = function () {
+    var fields = arguments;
+    return function (collection) {
+        var newCollection = [];
+        for (var i = 0; i < collection.length; i++) {
+            var contact = collection[i];
+            var newContact = {};
+            for (var j = 0; j < fields.length; j++) {
+                var field = fields[j];
+                newContact[field] = contact[field];
+            }
+            newCollection.push(newContact);
+        }
+        return newCollection;
+    };
+};
 
-// Будет круто, если реализуете операторы:
-// or и and
+module.exports.filterIn = function (field, possibleValues) {
+    return function (collection) {
+        var newCollection = [];
+        for (var i = 0; i < collection.length; i++) {
+            var contact = collection[i];
+            if (possibleValues.indexOf(contact[field]) >= 0) {
+                newCollection.push(contact);
+            }
+        }
+        return newCollection;
+    };
+};
+
+module.exports.filterEqual = function (field, value) {
+    return function (collection) {
+        var newCollection = [];
+        for (var i = 0; i < collection.length; i++) {
+            var contact = collection[i];
+            if (contact[field] === value) {
+                newCollection.push(contact);
+            }
+        }
+        return newCollection;
+    };
+};
+
+module.exports.sortBy = function (field, sortType) {
+    return function (collection) {
+        var sortFunction = function (a, b) {
+            if (a[field] > b[field]) {
+                return 1;
+            }
+            if (a[field] < b[field]) {
+                return -1;
+            }
+            return 0;
+        };
+        var sortedCollection = collection.sort(sortFunction);
+        if (sortType === 'desc') {
+            sortedCollection = sortedCollection.reverse();
+        }
+        return sortedCollection;
+    };
+};
+
+module.exports.format = function (field, changingFunction) {
+    return function (collection) {
+        for (var i = 0; i < collection.length; i++) {
+            var contact = collection[i];
+            contact[field] = changingFunction(contact[field]);
+        }
+        return collection;
+    };
+};
