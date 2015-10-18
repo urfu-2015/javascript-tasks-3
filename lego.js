9580620b90a56fd98dcc0d6c922e0400b7544024
Даниил <Tkach@MacBook-Air-Daniil.local>
2015-10-18 19:38:01 +0500

    add solution

--- lego.js
+++ lego.js
@@ -2,7 +2,13 @@
 
 // Метод, который будет выполнять операции над коллекцией один за другим
 module.exports.query = function (collection /* операторы через запятую */) {
+	var phoneBook = collection;
 
+	for (var i = 1; i < arguments.length; i++) {
+		phoneBook = arguments[i](phoneBook);
+	}
+	console.log(phoneBook);
+	return phoneBook;
 };
 
 // Оператор reverse, который переворачивает коллекцию
@@ -17,7 +23,78 @@ module.exports.reverse = function () {
 
 // Оператор limit, который выбирает первые N записей
 module.exports.limit = function (n) {
-    // Магия
+    return function (collection){
+    	return collection.slice(0, n);
+    };
+};
+
+module.exports.select = function (){
+	var values = [].slice.call(arguments);
+
+	return function	(collection){
+		var newPhoneBook = [];
+
+		for (var i = 0; i < collection.length; i++) {
+			var record = collection[i];
+			var newRecord = {};
+
+			for (var key in record) {
+				if (values.indexOf(key) !== -1) {
+					newRecord[key] = record[key];
+				}
+			}
+			newPhoneBook.push(newRecord);
+		}
+
+		return newPhoneBook;
+		};
+	};
+
+module.exports.filterIn = function (field, choice) {
+	return function (collection) {
+		var newPhoneBook = [];
+		for (var i = 0; i < collection.length; i++){
+			var record = collection[i];
+			var keys = Object.keys(record);
+
+			if (keys.indexOf(field) !== -1 && choice.indexOf(record[field]) !== -1){
+				newPhoneBook.push(record);
+			}
+		}
+		return newPhoneBook;
+	};
+};
+
+module.exports.filterEqual = function (key, value) {
+	return module.exports.filterIn(key, [value]);
+};
+
+module.exports.sortBy = function (key, method) {
+	return function (collection) {
+		var newPhoneBook = collection.sort(function (elementFirst, elementSecond) {
+			if (elementFirst[key] < elementSecond[key]) {
+				return -1;
+			}
+			if (elementFirst[key] > elementSecond[key]) {
+				return 1;
+			}
+			return 0; 
+		});
+		if (method === 'desc') {
+			return newPhoneBook.reverse();
+		}
+		return newPhoneBook;
+	};
+};
+
+module.exports.format = function(field, func) {
+    return function (collection) {
+        var newPhoneBook = collection.map(function(user) {
+            user[field] = func(user[field]);
+            return user;
+        });
+        return newPhoneBook;
+    };
 };
 
 // Вам необходимо реализовать остальные операторы:
