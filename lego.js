b717dbbd9d56eb9b0832c4439f96992202fad03d
Гатиятуллин Вячеслав <Aspromist@gmail.com>
2015-10-19 15:37:04 +0500

    Add js-task-3

--- lego.js
+++ lego.js
@@ -2,8 +2,25 @@
 
 // Метод, который будет выполнять операции над коллекцией один за другим
 module.exports.query = function (collection /* операторы через запятую */) {
+    for (var i = 1; i < arguments.length; ++i) {
+            collection = arguments[i](collection);
+    }
+        console.log(collection);
+}
 
-};
+module.exports.select = function () {
+    var filters = [].slice.call(arguments);
+    return function (collection) {
+        return collection.map(function (obj) {
+            for (var key in obj) {
+                if (filters.indexOf(key) === -1) {
+                    delete obj[key];
+                }
+            }
+            return obj;
+        });
+    }
+}
 
 // Оператор reverse, который переворачивает коллекцию
 module.exports.reverse = function () {
@@ -12,16 +29,52 @@ module.exports.reverse = function () {
 
         // Возращаем изменённую коллекцию
         return changedCollection;
-    };
-};
+    }
+}
+
+module.exports.filterIn = function (key, filters) {
+    return function (collection) {
+        return collection.filter(function (obj) {
+            return filters.indexOf(obj[key]) !== -1;
+        });
+    }
+}
+
+module.exports.filterEqual = function (key, filter) {
+    return function (collection) {
+        return collection.filter(function (obj) {
+            return obj[key] === filter;
+        });
+    }
+}
+
+module.exports.sortBy = function (key, order) {
+    return function (collection) {
+        if (order === 'asc') {
+            return collection.sort(function (obj1, obj2) {
+                return obj1[key] >= obj2[key] ? 1 : -1;
+            });
+        } else {
+            return collection.sort(function (obj1, obj2) {
+                return obj1[key] <= obj2[key] ? 1 : -1;
+            });
+        }
+    }
+}
+
+module.exports.format = function (key, filter) {
+    return function (collection) {
+        return collection.map(function (obj) {
+            obj[key] = filter(obj[key]);
+            return obj;
+        });
+    }
+}
 
 // Оператор limit, который выбирает первые N записей
 module.exports.limit = function (n) {
     // Магия
-};
-
-// Вам необходимо реализовать остальные операторы:
-// select, filterIn, filterEqual, sortBy, format, limit
-
-// Будет круто, если реализуете операторы:
-// or и and
+    return function (collection) {
+        return collection.slice(0, n);
+    }
+}
