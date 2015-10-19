e92807b04ad1ef892317aac383e926fa80161bca
BalakinVladislav <balakinv95@gmail.com>
2015-10-19 23:50:14 +0500

    almostready

--- lego.js
+++ lego.js
@@ -2,8 +2,12 @@
 
 // Метод, который будет выполнять операции над коллекцией один за другим
 module.exports.query = function (collection /* операторы через запятую */) {
-
-};
+	for (var i = 1; i < arguments.length; i++) {
+		collection = arguments[i](collection);
+	}
+	return collection;
+ };
+ 
 
 // Оператор reverse, который переворачивает коллекцию
 module.exports.reverse = function () {
@@ -17,11 +21,68 @@ module.exports.reverse = function () {
 
 // Оператор limit, который выбирает первые N записей
 module.exports.limit = function (n) {
+	return function(collection) {
+		var changedCollection = collection.slice(0,n);
+		return changedCollection;
+	}
     // Магия
 };
 
-// Вам необходимо реализовать остальные операторы:
-// select, filterIn, filterEqual, sortBy, format, limit
+/*module.exports.select = function ('name', 'gender', 'age', 'phone', 'favoriteFruit') {
+	var fields = [].slice.call(arguments);
+	return function(collection) {
+		for (i = 0; i < .length; i++) {
+			for (var key in collection[i]) {
+				if 
+			}
+			
+				
+			};
+			delete argument[i].collection
+		};
+		return collection
+	}
+    // Магия
+};*/
 
-// Будет круто, если реализуете операторы:
-// or и and
+module.exports.filterIn = function (property,values) {
+	return function(collection) {
+		var changedCollection = collection.filter(function(filterByValues) {
+			for (var i=0; i < values.length; i++) {
+				if (filterByValues[property] === values[i]) {
+					return true;
+					break;
+				}	
+			}
+			return false;
+		});
+		return changedCollection;
+	}
+
+    // Магия
+};
+
+module.exports.format = function (property,func) {
+	return function(collection) {
+		var changedCollection = collection.map(function(record) {
+			record[property] = func(record[property]);
+			return record;
+		}
+	return changedCollection;
+	};
+};
+
+module.exports.sortBy = function (property,howSort) {
+	return function(collection) {
+		var changedCollection = collection.sort(function(firstRec,secondRec) {
+			return [firstRec[property],secondRec[property]];
+		});
+		return if (howSort ==='asc') {
+			changedCollection;
+		} else {
+			changedCollection.reverse;
+		}
+	};
+};
+
+
