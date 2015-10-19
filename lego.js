7107259a2e3191350e76d3994f7fe544bcc27065
Nastya01195 <nastya01195@mail.ru>
2015-10-19 19:55:54 +0500

    Add solution

--- lego.js
+++ lego.js
@@ -1,27 +1,92 @@
 'use strict';
 
-// Метод, который будет выполнять операции над коллекцией один за другим
-module.exports.query = function (collection /* операторы через запятую */) {
-
+module.exports.query = function (collection) {
+    for(var i = 1; i < arguments.length; i++){
+        collection = arguments[i](collection);
+    }
+    return collection;
 };
 
-// Оператор reverse, который переворачивает коллекцию
 module.exports.reverse = function () {
     return function (collection) {
         var changedCollection = collection.reverse();
-
-        // Возращаем изменённую коллекцию
         return changedCollection;
     };
 };
 
-// Оператор limit, который выбирает первые N записей
 module.exports.limit = function (n) {
-    // Магия
+    return function(collection){
+        return collection.slice(0, n);
+    }
 };
 
-// Вам необходимо реализовать остальные операторы:
-// select, filterIn, filterEqual, sortBy, format, limit
+module.exports.select = function (){
+    var fields = [].slice.call(arguments);
+    return function(collection){
+        var newCollection = [];
+        for(var i = 0; i < collection.length; i++){
+            var contact = collection[i];
+            var newContact = {};
+            for(var key in contact){
+                if(fields.indexOf(key) !== -1){
+                    newContact[key] = contact[key];
+                }
+            }
+            newCollection.push(newContact);
+        }
+        return newCollection;
+    }
+}
 
-// Будет круто, если реализуете операторы:
-// or и and
+module.exports.filterIn = function (field, values){
+    return function(collection){
+        var newCollection = [];
+        for(var i = 0; i < collection.length; i++){
+            var contact = collection[i];
+            for(var key in contact){
+                if(key === field && values.indexOf(contact[key]) != -1){
+                   newCollection.push(contact);
+                }
+            }
+        }
+        return newCollection;
+    }
+}
+
+module.exports.filterEqual = function (field, value){
+    return function(collection){
+        return module.exports.filterIn(field, [value]);
+    }
+}
+
+module.exports.sortBy=function(field, parametr){
+    return function(collection){
+        var newCollection = collection.sort(function(a, b){
+            if(a[field] > b[field]){
+                return 1;
+            }
+            if(a[field] < b[field]){
+                return -1;
+            }
+        });
+        if(parametr === 'desc'){
+            return newCollection.reverse();
+        }
+        return newCollection;
+    }
+}
+
+module.exports.format=function(field, func){
+    return function(collection){
+        var newCollection = [];
+        for(var i = 0; i < collection.length; i++){
+            var contact = collection[i];
+            var newContact = {};
+            for(var key in contact){
+                newContact[key] = key === field ? func(contact[key]) : contact[key];
+            }
+            newCollection.push(newContact);
+        }
+        return newCollection;
+    }
+}
