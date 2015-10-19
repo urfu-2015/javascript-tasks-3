826d998fe105143f73bbcc6aa18c25894dc6e07d
Grehhov <grehhov@mail.ru>
2015-10-19 17:03:41 +0500

    Update lego.js

--- lego.js
+++ lego.js
@@ -1,8 +1,12 @@
 'use strict';
-
 // Метод, который будет выполнять операции над коллекцией один за другим
 module.exports.query = function (collection /* операторы через запятую */) {
-
+	var phoneBook = collection;
+	for (var i = 1; i < arguments.length; i++) {
+		phoneBook = arguments[i](phoneBook);
+	}
+	console.log(phoneBook);
+	return phoneBook;
 };
 
 // Оператор reverse, который переворачивает коллекцию
@@ -15,11 +19,88 @@ module.exports.reverse = function () {
     };
 };
 
+module.exports.select = function () {
+	var arg = [].slice.apply(arguments);
+	return function (collection) {
+		var selectedPhoneBook = [];
+		for (var i = 0; i < collection.length; i++) {
+			selectedPhoneBook[i] = {};
+			for(var item in arg) {
+				selectedPhoneBook[i][arg[item]] = collection[i][arg[item]];
+			}
+		}
+		return selectedPhoneBook;
+	}
+}
+
+module.exports.filterIn = function (item, filter) {
+	return function (collection) {
+		var filtredPhoneBook = [];
+		for (var i = 0; i < collection.length; i++) {
+			for(var e in filter) {
+				if (collection[i][item] === filter[e]) {
+					filtredPhoneBook.push(collection[i]);
+					break;
+				}
+			}
+		}
+		return filtredPhoneBook;
+	}
+}
+
+module.exports.filterEqual = function (item, filter) {
+	return function (collection) {
+		var filtredPhoneBook = [];
+		for (var i = 0; i < collection.length; i++) {
+			if (collection[i][item] === filter) {
+				filtredPhoneBook.push(collection[i]);
+			}
+		}
+		return filtredPhoneBook;
+	}
+}
+
+
+module.exports.sortBy = function (item, isReverse) {
+	return function (collection) {
+		function compare(a, b) {
+			if (a[item] > b[item]) {
+				return 1;
+			}
+			if (a[item] < b[item]) {
+				return -1;
+			}
+			return 0;
+		}
+		
+		collection = collection.sort(compare);
+		if (isReverse === 'desc') {
+			return collection.reverse();
+		}
+		return collection;
+	}
+}
+
+
+module.exports.format = function (item, func) {
+	return function (collection) {
+		for (var i = 0; i < collection.length; i++) {
+			collection[i][item] = func(collection[i][item]);
+		}
+		return collection;
+	}
+}
+
 // Оператор limit, который выбирает первые N записей
 module.exports.limit = function (n) {
-    // Магия
+	return function (collection) {
+		if (n < collection.length) {
+			return collection.slice(0, n);
+		}
+		return collection;
+	}
+	// Магия
 };
-
 // Вам необходимо реализовать остальные операторы:
 // select, filterIn, filterEqual, sortBy, format, limit
 
