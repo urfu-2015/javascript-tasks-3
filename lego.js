eedc21229b826903311fc73e2e9498159e96d367
Anna Kuznetsova <ann_v_smith@bk.ru>
2015-10-19 22:03:50 +0500

    Update lego.js

--- lego.js
+++ lego.js
@@ -1,27 +1,101 @@
 'use strict';
 
 // Метод, который будет выполнять операции над коллекцией один за другим
-module.exports.query = function (collection /* операторы через запятую */) {
-
+module.exports.query = function (collection) {
+    var argsLength = arguments.length;
+    for (var i = 1; i < argsLength; i++) {
+        collection = arguments[i](collection);
+    }
+    return collection;
 };
 
 // Оператор reverse, который переворачивает коллекцию
 module.exports.reverse = function () {
     return function (collection) {
-        var changedCollection = collection.reverse();
-
-        // Возращаем изменённую коллекцию
-        return changedCollection;
+        return collection.reverse();
     };
 };
 
 // Оператор limit, который выбирает первые N записей
 module.exports.limit = function (n) {
-    // Магия
+    return function (collection) {
+        if (n < 0) {
+            throw 'The number of entries must be positive';
+        }
+        return n < collection.length ? collection.slice(0, n) : collection;
+    };
 };
 
-// Вам необходимо реализовать остальные операторы:
-// select, filterIn, filterEqual, sortBy, format, limit
+// Оператор select, который выбирает определенные поля из телефонной книги
+module.exports.select = function () {
+    var fields = [].slice.call(arguments);
+    return function (collection) {
+        var newCollection = [];
+        collection.forEach(function (record, recordIndex) {
+            newCollection[recordIndex] = {};
+            fields.forEach(function (field) {
+                if (field in record) {
+                    newCollection[recordIndex][field] = collection[recordIndex][field];
+                }
+            });
+        });
+        return newCollection;
+    };
+};
 
-// Будет круто, если реализуете операторы:
-// or и and
+// Оператор filterIn фильтрует по значению в некотором поле
+module.exports.filterIn = function (fieldName, values) {
+    return function (collection) {
+        return collection.filter(function (record) {
+            return values.indexOf(record[fieldName]) !== -1;
+        });
+    };
+};
+
+// Оператор filterEqual
+module.exports.filterEqual = function (fieldName, value) {
+    return function (collection) {
+        return collection.filter(function (record) {
+            return record[fieldName] === value;
+        });
+    };
+};
+
+// Оператор sortBy, сортирует по какому-то полю
+module.exports.sortBy = function (fieldName, sortType) {
+    return function (collection) {
+        if (!(sortType === 'asc' || sortType === 'desc')) {
+            throw 'Incorrect sorting method. Possible options: \'asc\', \'desc\'';
+        }
+        if (sortType === 'asc') {
+            return collection.sort(function (a, b) {
+                if (a[fieldName] > b[fieldName]) {
+                    return 1;
+                }
+                if (a[fieldName] < b[fieldName]) {
+                    return -1;
+                }
+                return 0;
+            });
+        }
+        return collection.sort(function (a, b) {
+            if (a[fieldName] > b[fieldName]) {
+                return -1;
+            }
+            if (a[fieldName] < b[fieldName]) {
+                return 1;
+            }
+            return 0;
+        });
+    };
+};
+
+// Оператор format - форматирует некоторое поле
+module.exports.format = function (fieldName, formatFunction) {
+    return function (collection) {
+        collection.forEach(function (record) {
+            record[fieldName] = formatFunction(record[fieldName]);
+        });
+        return collection;
+    };
+};
