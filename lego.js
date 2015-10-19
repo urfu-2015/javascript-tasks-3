b9c7b41ffac85eb6720948c03f1cb5537b390ad1
Dolgih Natalia <dolgihnatasha@users.noreply.github.com>
2015-10-19 21:40:52 +0500

    Update lego.js

--- lego.js
+++ lego.js
@@ -1,27 +1,126 @@
 'use strict';
 
-// Метод, который будет выполнять операции над коллекцией один за другим
-module.exports.query = function (collection /* операторы через запятую */) {
 
+module.exports.query = function (collection) {
+    var args = [].slice.call(arguments, 0);
+    for(var i=1;i<args.length;i++){
+        collection = args[i](collection);
+    }
+    return collection;
 };
 
-// Оператор reverse, который переворачивает коллекцию
+module.exports.select = function () {
+    var selected = [].slice.call(arguments, 0);
+    return function (collection){
+        var result = [];
+        for (var i=0; i<collection.length; i++){
+            result.push({});
+            for(var e in collection[i]){
+                if(selected.indexOf(e) != -1){
+                    result[i][e] = collection[i][e]
+                }
+            }
+        }
+        return result;
+    }
+};
+
+module.exports.filterIn = function () {
+    var args = [].slice.call(arguments, 0);
+    var field = args[0];
+    args = args[1];
+    return function (collection){
+        return collection.filter(function(entry){
+            return args.indexOf(entry[field]) !== -1;
+        });
+    }
+};
+
+module.exports.filterEqual = function () {
+    var args = [].slice.call(arguments, 0);
+    var field = args[0];
+    args = args[1];
+    return function (collection){
+        return collection.filter(function(entry){
+            return entry[field] === args;
+        });
+    }
+};
+
+module.exports.sortBy = function () {
+    var args = [].slice.call(arguments, 0);
+    var field = args[0];
+    var f = args[1];
+    return function (collection){
+        return collection.sort(function(entry1, entry2){
+            if(entry1[field] > entry2[field]){
+                if (f === 'asc')
+                    return 1;
+                else
+                    return -1;
+            }
+            if(entry1[field] < entry2[field]){
+                if (f === 'asc')
+                    return -1;
+                else
+                    return 1;
+            }
+            return 0;
+        });
+    }
+};
+
+module.exports.format = function () {
+    var args = [].slice.call(arguments, 0);
+    var field = args[0];
+    var f = args[1];
+    return function (collection){
+        return collection.map(function(entry){
+            entry[field] = f(entry[field]);
+            return entry;
+        });
+    }
+};
+
+module.exports.limit = function () {
+    var n = arguments[0];
+    return function (collection){
+        return collection.slice(0,n);
+    }
+};
+
 module.exports.reverse = function () {
     return function (collection) {
-        var changedCollection = collection.reverse();
-
-        // Возращаем изменённую коллекцию
-        return changedCollection;
+        return collection.slice().reverse();
     };
 };
 
-// Оператор limit, который выбирает первые N записей
-module.exports.limit = function (n) {
-    // Магия
+module.exports.or = function () {
+    var args = [].slice.call(arguments, 0);
+    return function (collection) {
+        var tempCollection = [];
+        for(var i=0;i<args.length;i++) {
+            var x = args[i](collection);
+            tempCollection = tempCollection.concat(x);
+        }
+        collection = [];
+        for(i=0; i<tempCollection.length; ++i) {
+            if(collection.indexOf(tempCollection[i]) === -1){
+                collection.push(tempCollection[i]);
+            }
+        }
+        return collection;
+    };
 };
 
-// Вам необходимо реализовать остальные операторы:
-// select, filterIn, filterEqual, sortBy, format, limit
+module.exports.and = function () {
+    var args = [].slice.call(arguments, 0);
+    return function (collection) {
+        for(var i=0;i<args.length;i++){
+            collection = args[i](collection);
+        }
+        return collection;
+    };
+};
 
-// Будет круто, если реализуете операторы:
-// or и and
+
