4fc88706ea666189f611521f5df587ffe825cfc9
Rybalka Ekaterina <lonelygopnik@gmail.com>
2015-10-23 22:13:14 +0500

    Поправки

--- lego.js
+++ lego.js
@@ -21,109 +21,111 @@ module.exports.reverse = function () {
 // Оператор limit, который выбирает первые N записей
 module.exports.limit = function (n) {
     // Магия
-    return function(collection){
-        if (collection.length <= n)
-            return collection;
+    return function (collection) {
+        if (n < 0) {
+            return [];
+        }
         return collection.slice(0, n);
-    }
+    };
 };
 
 // Вам необходимо реализовать остальные операторы:
 // select, filterIn, filterEqual, sortBy, format, limit
 
-module.exports.select = function(){
+module.exports.select = function () {
     var fields = [];
     for (var i = 0; i < arguments.length; i++) {
         fields[i] = arguments[i];
     }
-    return function(collection){
-        return collection.map(function(note){
+    return function (collection) {
+        return collection.map(function (note) {
             var changedNote = {};
-            fields.forEach(function(field){
-                if (field in note)
+            fields.forEach(function (field) {
+                if (field in note) {
                     changedNote[field] = note[field];
+                }
             });
             return changedNote;
         });
-    }
+    };
 };
 
-module.exports.filterIn = function(field, values){
-    return function(collection) {
-        var changedCollection = [];
-        collection.forEach(function (note) {
-            values.forEach(function (value) {
-                if (note[field] == value)
-                    changedCollection.push(note);
-            })
+module.exports.filterIn = function (field, values) {
+    return function (collection) {
+        return collection.filter(function (note) {
+            return values.indexOf(note[field]) >= 0;
         });
-        return changedCollection;
-    }
+    };
 };
 
-module.exports.filterEqual = function(field, value){
-    return function(collection){
-        return collection.filter(function(note){
+module.exports.filterEqual = function (field, value) {
+    return function (collection) {
+        return collection.filter(function (note) {
             return note[field] == value;
         });
-    }
+    };
 };
 
-module.exports.sortBy = function(field, method){
-    return function(collection){
-        function sortCol(a, b){
-            if(a[field] < b[field])
+module.exports.sortBy = function (field, method) {
+    return function (collection) {
+        function sortCol(a, b) {
+            if (a[field] < b[field]) {
                 return -1;
-            if(a[field] > b[field])
+            } else if (a[field] > b[field]) {
                 return 1;
+            }
             return 0;
         }
 
-        if (method == 'asc')
+        if (method == 'asc') {
             return collection.sort(sortCol);
-        else if (method == 'desc')
+        } else if (method == 'desc') {
             return collection.sort(sortCol).reverse();
-    }
+        }
+    };
 };
 
-module.exports.format = function(field, func){
-    return function(collection) {
-        return collection.map(function(note){
+module.exports.format = function (field, func) {
+    return function (collection) {
+        return collection.map(function (note) {
             note[field] = func(note[field]);
             return note;
         });
-    }
+    };
 };
 
 // Будет круто, если реализуете операторы:
 // or и and
-function equalsNotes(a, b){
+function equalsNotes(a, b) {
     var result = false;
-    for (var keyA in a){
-        for (var keyB in b){
-            if (keyA == keyB && a[keyA] == b[keyB])
-                result = true;
-            else
-                break;
+    for (var keyA in a) {
+        for (var keyB in b) {
+            if (keyA == keyB) {
+                if (a[keyA] == b[keyB]) {
+                    result = true;
+                    break;
+                }
+            }
         }
     }
     return result;
 }
 
-function mergerCollections(a, b){
+function mergerCollections(a, b) {
     var result = a;
-    var bool = false;
-    b.forEach(function(noteB){
+    var bool;
+
+    b.forEach(function (noteB) {
+        bool = false;
         for (var noteA in a) {
             if (equalsNotes(noteA, noteB)) {
                 bool = true;
                 break;
             }
         }
-        if (!bool){
+        if (!bool) {
             result.push(noteB);
-            var i = b.indexOf(noteB);
-            b.slice(i, 1);
+            b.slice(b.indexOf(noteB), 1);
         }
     });
     return result;
@@ -131,23 +133,22 @@ function mergerCollections(a, b){
 
 module.exports.or = function () {
     var arg = arguments;
-    return function(collection) {
+    return function (collection) {
         var resultCollection = [];
-        var col = [];
         for (var i = 0; i < arg.length; i++) {
-            col = arg[i](collection);
-            resultCollection = mergerCollections(resultCollection, col);
+            resultCollection = mergerCollections(resultCollection, arg[i](collection));
+            console.log(resultCollection);
         }
         return resultCollection;
-    }
+    };
 };
 
-module.exports.and = function(){
+module.exports.and = function () {
     var arg = arguments;
-    return function(collection) {
+    return function (collection) {
         for (var i = 0; i < arg.length; i++) {
             collection = arg[i](collection);
         }
         return collection;
-    }
+    };
 };
