ed32058254261291bd90caa9a7cadadd0c95db0e
Vedmax <max.vedernikov@gmail.com>
2015-10-23 23:04:21 +0500

    fixed mistakes

--- lego.js
+++ lego.js
@@ -9,23 +9,18 @@ module.exports.query = function (collection /* операторы через з�
     return resultCollection;
 };
 
-function inArgs(args, prop) {
-    for (var i = 0; i < args.length; i++) {
-        if (args[i] === prop) {
-            return true;
-        }
-    }
-    return false;
-}
-
 module.exports.select = function () {
-    var args = arguments;
+    var args = [];
+    for (var i = 0; i < arguments.length; i++) {
+        args.push(arguments[i]);
+    }
+    args.join();
     return function (collection) {
         var changedCollection = [];
         for (var i = 0; i < collection.length; i++) {
             var newObj = {};
             for (var prop in collection[i]) {
-                if (inArgs(args, prop)) {
+                if (args.indexOf(prop.toString()) !== -1) {
                     newObj[prop] = collection[i][prop];
                 }
             }
@@ -44,24 +39,12 @@ module.exports.reverse = function () {
     };
 };
 
-function isInVariant(field, args, person) {
-    var argsString = args.join();
-    for (var param in person) {
-        if (param === field) {
-            if (argsString.indexOf(person[param]) != -1) {
-                return true;
-            }
-        }
-    }
-    return false;
-}
-
-module.exports.filterIn = function () {
-    var args = arguments;
+module.exports.filterIn = function (propName, variants) {
     return function (collection) {
+        variants = variants.join();
         var changedCollection = [];
         for (var i = 0; i < collection.length; i++) {
-            if (isInVariant(args[0], args[1], collection[i])) {
+            if (variants.indexOf(collection[i][propName]) !== -1) {
                 changedCollection.push(collection[i]);
             }
         }
@@ -72,23 +55,24 @@ module.exports.filterIn = function () {
 
 module.exports.sortBy = function (param, order) {
     return function (collection) {
-        collection.sort(function (a, b) {
+        order = !order || order === 'asc' ? true : false;
+        return collection.sort(function (a, b) {
             if (a[param] < b[param]) {
-                return -1;
+                return order ? -1 : 1;
             }
             if (a[param] > b[param]) {
-                return 1;
+                return order ? 1 : -1;
             }
             return 0;
         });
-        return order === 'asc' ? collection : collection.reverse();
     };
 };
 
 // Оператор limit, который выбирает первые N записей
 module.exports.limit = function (n) {
     return function (collection) {
-        return collection.slice(0, n);
+        var len = collection.length;
+        return collection.slice(0, Math.max(n % len, 0));
     };
     // Магия
 };
@@ -99,11 +83,8 @@ module.exports.format = function (property, func) {
         for (var i = 0; i < collection.length; i++) {
             var newObj = {};
             for (var key in collection[i]) {
-                if (key === property) {
-                    newObj[key] = func(collection[i][key]);
-                } else {
-                    newObj[key] = collection[i][key];
-                }
+                newObj[key] = key === property ?
+                    func(collection[i][key]) : collection[i][key];
             }
             changedCollection.push(newObj);
         }
@@ -112,20 +93,17 @@ module.exports.format = function (property, func) {
     // Магия
 };
 
-
-
-module.exports.filterEqual = function (pararmetr, value) {
+module.exports.filterEqual = function (param, value) {
     return function (collection) {
         var changedCollection = [];
         for (var i = 0; i < collection.length; i++) {
-            if (collection[i][pararmetr] === value) {
+            if (collection[i][param] === value) {
                 changedCollection.push(collection[i]);
             }
         }
         return changedCollection;
     };
 };
-
 
 // Вам необходимо реализовать остальные операторы:
 // select, filterIn, filterEqual, sortBy, format, limit
@@ -143,26 +121,40 @@ module.exports.and = function () {
     };
 };
 
-function addElem(collection, dict, elem) {
-    var str = [elem['age'], elem['gender'], elem['name'],
-        elem['email'], elem['phone'], elem['favoriteFruit']].join();
-    if (dict[str] === undefined) {
-        collection.push(elem);
-        dict[str] = 1;
-    }
-}
-
 module.exports.or = function () {
     var functions = arguments;
     return function (collection) {
         var newCollection = [];
-        var dict = {};
         for (var i = 0; i < functions.length; i++) {
             var funcResult = functions[i](collection);
             for (var j = 0; j < funcResult.length; j++) {
-                addElem(newCollection, dict, funcResult[j]);
+                addElem(newCollection, funcResult[j]);
             }
         }
         return newCollection;
     };
 };
+
+function inArgs(args, prop) {
+    for (var i = 0; i < args.length; i++) {
+        if (args[i] === prop) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function addElem(collection, elem) {
+    for (var i = 0; i < collection.length; i++) {
+        var eq = true;
+        for (var prop in collection[i]) {
+            if (elem[prop] !== collection[i][prop]) {
+                eq = false;
+            }
+        }
+        if (eq) {
+            return;
+        }
+    }
+    collection.push(elem);
+}
