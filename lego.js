96098d058dae2ceff53996ee8ca6fd6863c15110
Minniezator <minniezator17@yandex.ru>
2015-10-25 02:33:33 +0500

    Update lego.js

--- lego.js
+++ lego.js
@@ -21,8 +21,11 @@ module.exports.reverse = function () {
 // Оператор limit, который выбирает первые N записей
 module.exports.limit = function (n) {
     return function (collection) {
-        collection = collection.slice(0, n);
-        return collection;
+        if (!isFinite(n) || n <= 0) {
+            return [];
+        } else {
+            return collection.slice(0, n);
+        }
     };
 };
 
@@ -31,60 +34,56 @@ module.exports.limit = function (n) {
 module.exports.select = function (field) {
     var keys = arguments;
     return function (collection) {
-        collection = collection.map(function (contact) {
+        return collection.map(function (contact) {
             var newObject = {};
             for (var i = 0; i < keys.length; ++i) {
                 newObject[keys[i]] = contact[keys[i]];
             }
             return newObject;
         });
-        return collection;
     };
 };
 
 module.exports.filterIn = function (field, condition) {
     return function (collection) {
-        collection = collection.filter(function (contact) {
+        return collection.filter(function (contact) {
             for (var i in condition) {
-                if (contact[field] === condition[i]) {
+                if (contact[field].toLowerCase() === condition[i].toLowerCase()) {
                     return true;
                 }
             }
             return false;
         });
-        return collection;
     };
 };
 
 module.exports.filterEqual = function () {
     return function (collection) {
-        collection = collection.filter(function (contact) {
-            return contact[field] === condition ? true : false;
+        return collection.filter(function (contact) {
+            return contact[field].toLowerCase() === condition.toLowerCase() ? true : false;
         });
-        return collection;
     };
 };
 
 module.exports.sortBy = function (field, condition) {
     return function (collection) {
         collection = collection.sort(function (a, b) {
-            if (a[field] > b[field]) {
-                return 1;
-            } else {
-                return -1;
-            }
+            return a[field] - b[field];
         });
         return condition === 'asc' ? collection : collection.reverse();
     };
 };
 
-module.exports.format = function (field, getFirstLetter) {
+module.exports.format = function (field, filterFinction) {
     return function (collection) {
-        collection = collection.map(function (contact) {
-            contact[field] = getFirstLetter(contact[field]);
-            return contact;
-        });
-        return collection;
+        if (collection[0].hasOwnProperty(field)) {
+            return collection.map(function (contact) {
+                contact[field] = filterFinction(contact[field]);
+                return contact;
+            });
+        } else {
+            return [];
+        }
     };
 };
 
