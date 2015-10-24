73255ca9da21eb8e5affa9a158b6244c65ecf9a7
v-samoylov <v-samoylov@users.noreply.github.com>
2015-10-24 15:05:06 +0500

    Update lego.js

--- lego.js
+++ lego.js
@@ -28,11 +28,11 @@ module.exports.select = function () {
         var changedCollection = collection.map(function (entry){
             var newEntry = {};
             var entryProps = Object.keys(entry);
-            entryProps.forEach(function (prop){
-                if (propList.has(prop)) {
-                    newEntry[prop] = entry[prop];
+            entryProps.reduce(function (previousProp, currentProp){
+                if (propList.has(currentProp)) {
+                    newEntry[currentProp] = entry[currentProp];
                 }
-            });
+            }, 0);
             return newEntry;
         });
         return changedCollection;
@@ -68,29 +68,24 @@ module.exports.filterEqual = function (property, propValue) {
 module.exports.sortBy = function (property, order) {
     var compare = function (a, b) {
         if (order === 'asc') {
-            return a > b;
+            return a - b;
         }
         if (order === 'desc') {
-            return a < b;
+            return b - a;
         }
     }
     return function (collection) {
         var changedCollection = [].concat(collection);
         var isSorted = false;
         while (!isSorted) {
-            isSorted = true;
-            for (var i = 0; i < changedCollection.length - 1; i++) {
-                var collEntry1 = changedCollection[i];
-                var collEntry2 = changedCollection[i+1];
-                var property1 = collEntry1[property];
-                var property2 = collEntry2[property];
-                if (compare(property1, property2)) {
-                    var collEntry1Copy = collEntry1;
-                    changedCollection.splice(i, 1);
-                    changedCollection.splice(i + 1, 0, collEntry1Copy);
+            changedCollection.sort(function (a, b) {
+                isSorted = true;
+                var comparison = compare(a[property], b[property]);
+                if (comparison > 0) {
                     isSorted = false;
                 }
-            }
+                return comparison;
+            });
         }
     return changedCollection;
     }
@@ -107,35 +102,26 @@ module.exports.format = function (property, _function) {
 }
 
 module.exports.or = function () {
-    var funcList = Object.assign(arguments);
+    var funcList = arguments;
     return function(collection) {
-        var changedCollection = new Set();
+        var changedCollection = [];
         for (var i = 0; i < funcList.length; i++) {
             var func = funcList[i];
             var funcResult = func(collection);
-            funcResult.forEach(function (entry) {
-                changedCollection.add(entry);
-            });
+            changedCollection = changedCollection.concat(funcResult);
         }
         return changedCollection;
-    } 
+    }
 }
 
 module.exports.and = function () {
-    var funcList = Object.assign(arguments);
+    var funcList = arguments;
     return function (collection) {
-        var func = funcList[0];
-        var funcResult = func(collection);
-        var commonEntries = new Set(funcResult);
-        for (var i = 1; i < funcList.length; i++) {
-            func = funcList[i];
-            var funcResult = func(collection);
-            commonEntries = funcResult.map(function (entry) {
-                if (commonEntries.has(entry)) {
-                    return entry;
-                }
-            });
+        var changedCollection = [].concat(collection);
+        for (var i = 0; i < funcList.length; i++) {
+            var func = funcList[i];
+            changedCollection = func(changedCollection);
         }
-        return commonEntries;
+        return changedCollection;
     }
 }
