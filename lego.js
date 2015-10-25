1e73054425cc6ca3b7b54152b6b1c1b2b8484809
paulkhomich <paulkhomich@outlook.com>
2015-10-25 16:59:30 +0500

    1.0.1

--- lego.js
+++ lego.js
@@ -15,21 +15,92 @@ module.exports.reverse = function () {
     };
 };
 
+//Оператор select, который выбирает из массива
+module.exports.select = function () {
+	var keys = [];
+	return function (collection) {
+		var filterCollection = [];
+		for(var i in collection){
+			filterCollection[i] = [];
+			keys.forEach(function(evade){
+				filterCollection[k][evade] = collection[k][evade];
+			});
+		}
+		return filterCollection;
+	};
+};
+
+// Оператор filterIn, который фильтрует по параметрам
+
+module.exports.filterIn = function (keyFilter, valueFilter) {
+	return function (collection) {
+		var filterCollection = [];
+		collection.forEach(function(evade){
+			valueFilter.forEach(function(value){
+				if(evade[keyForFilter] == value){
+					filterCollection.push(evade);
+				}
+			});
+		});
+		return filterCollection;
+	};	
+};
+
+// Оператор filterEqual, который фильтрует по конкретному парамтеру
+
+module.exports.filterEqual = function (keyFilter, valueFilter) {
+	return function (collection) {
+		var filterCollection = [];
+		collection.forEach(function(evade){
+			if(evade[keyFilter] == valueFilter){
+				filterCollection.push(evade);
+			}
+		});
+		return filterCollection;
+	};	
+};
+
+// Оператор sortBy, который сортирует по признаку
+
+module.exports.sortBy = function (keySort, evade) {
+	return function(collection){
+		function compared(a,b){
+			return a[keySort] > b[keySort] ? 1 : -1;
+		}
+		var sortCollection = collection.sort(compared);
+		return evade === 'ask' ? sortCollection : sortCollection.reverse();
+	};
+};
+
+
+// Оператор format, который форматирует функцию
+
+module.exports.format = function (key, func) {
+	return function (collection) {
+		collection.forEach(function(evade){
+			evade[key] = func(evade[key]);
+		});
+		return collection;
+	};	
+};
+
 // Оператор limit, который выбирает первые N записей
 module.exports.limit = function (n) {
     // Магия
-    var isIndexValid = true;
-    if (n < 0) {
-        console.error('Неверный индекс!');
-        isIndexValid = false;
-    }
     return function (collection) {
-        if (isValidIndex) {
-            return collection.slice(0, n);
-        } else {
-            return collection;
-        }
-    };
+	    var isIndexValid = true;
+	    if (n < 0) {
+	        console.error('Неверный индекс!');
+	        isIndexValid = false;
+	    }
+	    return function (collection) {
+	        if (isValidIndex) {
+	            return collection.slice(0, n);
+	        } else {
+	            return collection;
+	        }
+	    };
+	}; 
 };
 
 // Вам необходимо реализовать остальные операторы:
