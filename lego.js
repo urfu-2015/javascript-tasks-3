1d6f8d1039abf5fe459cbfa250330ef3b432012d
BalakinVladislav <balakinv95@gmail.com>
2015-10-27 00:12:25 +0500

    ready1

--- lego.js
+++ lego.js
@@ -1,13 +1,13 @@
 'use strict';
 
 // Метод, который будет выполнять операции над коллекцией один за другим
-module.exports.query = function (collection /* операторы через запятую */) {
-	for (var i = 1; i < arguments.length; i++) {
-		collection = arguments[i](collection);
-	}
-	return collection;
- };
- 
+module.exports.query = function (collection) {
+    for (var i = 1; i < arguments.length; i++) {
+        collection = arguments[i](collection);
+    }
+    return collection;
+};
+
 
 // Оператор reverse, который переворачивает коллекцию
 module.exports.reverse = function () {
@@ -19,70 +19,62 @@ module.exports.reverse = function () {
     };
 };
 
-// Оператор limit, который выбирает первые N записей
 module.exports.limit = function (n) {
-	return function(collection) {
-		var changedCollection = collection.slice(0,n);
-		return changedCollection;
-	}
-    // Магия
+    return function (collection) {
+        var changedCollection = collection.slice(0, n);
+        console.log(changedCollection);
+        return changedCollection;
+    };
 };
 
-/*module.exports.select = function ('name', 'gender', 'age', 'phone', 'favoriteFruit') {
-	var fields = [].slice.call(arguments);
-	return function(collection) {
-		for (i = 0; i < .length; i++) {
-			for (var key in collection[i]) {
-				if 
-			}
-			
-				
-			};
-			delete argument[i].collection
-		};
-		return collection
-	}
-    // Магия
-};*/
-
-module.exports.filterIn = function (property,values) {
-	return function(collection) {
-		var changedCollection = collection.filter(function(filterByValues) {
-			for (var i=0; i < values.length; i++) {
-				if (filterByValues[property] === values[i]) {
-					return true;
-					break;
-				}	
-			}
-			return false;
-		});
-		return changedCollection;
-	}
-
-    // Магия
+module.exports.select = function () {
+    var fields = [].slice.call(arguments);
+    return function (collection) {
+        return collection.map(function (record) {
+            for (var key in record) {
+                if (fields.indexOf(key) === -1) {
+                    delete record[key];
+                }
+            }
+            return record;
+        });
+    };
 };
 
-module.exports.format = function (property,func) {
-	return function(collection) {
-		var changedCollection = collection.map(function(record) {
-			record[property] = func(record[property]);
-			return record;
-		}
-	return changedCollection;
-	};
+module.exports.filterIn = function (property, values) {
+    return function (collection) {
+        var changedCollection = collection.filter(function (filterByValues) {
+            for (var i = 0; i < values.length; i++) {
+                if (filterByValues[property] === values[i]) {
+                    return true;
+                    break;
+                }
+            }
+            return false;
+        });
+        return changedCollection;
+    };
 };
 
-module.exports.sortBy = function (property,howSort) {
-	return function(collection) {
-		var changedCollection = collection.sort(function(firstRec,secondRec) {
-			return [firstRec[property],secondRec[property]];
-		});
-		return if (howSort ==='asc') {
-			changedCollection;
-		} else {
-			changedCollection.reverse;
-		}
-	};
+module.exports.format = function (property, func) {
+    return function (collection) {
+        var changedCollection = collection.map(function (record) {
+            record[property] = func(record[property]);
+            return record;
+        });
+        return changedCollection;
+    };
 };
 
+module.exports.sortBy = function (property, howSort) {
+    return function (collection) {
+        var changedCollection = collection.sort(function (firstRec, secondRec) {
+            return [firstRec[property],secondRec[property]];
+        });
+        return (howSort === 'asc') ? changedCollection : changedCollection.reverse();
+    };
+};
 
+module.exports.filterEqual = function (property, value) {
+    filterIn(property, [value]);
+};
