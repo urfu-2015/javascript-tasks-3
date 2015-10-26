d04de02347e9ece68d4ab51f74246bd709f3aa4f
VladimirIvanov93 <ivanov.vy3@gmail.com>
2015-10-26 22:45:49 +0500

    Update lego.js

--- lego.js
+++ lego.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Метод, который будет выполнять операции над коллекцией один за другим
-module.exports.query = function(collection) {
+module.exports.query = function (collection) {
     for (var i = 1; i < arguments.length; i++) {
         collection = arguments[i](collection);
     }
@@ -9,17 +9,17 @@ module.exports.query = function(collection) {
 };
 
 // Оператор reverse, который переворачивает коллекцию
-module.exports.reverse = function() {
-    return function(collection) {
-        return collection.slice().reverse();
+module.exports.reverse = function () {
+    return function (collection) {
+        return collection.reverse();
     };
 };
 
 // Оператор limit, который выбирает первые N записей
-module.exports.limit = function(n) {
-    return function(collection) {
+module.exports.limit = function (n) {
+    return function (collection) {
         if (n < 0) {
-            throw new new RangeError('Should`t be limited array negative number.');
+            throw new RangeError('Should`t be limited array negative number.');
         }
         return collection.slice(0, n);
     };
@@ -27,10 +27,10 @@ module.exports.limit = function(n) {
 
 // Вам необходимо реализовать остальные операторы:
 // select, filterIn, filterEqual, sortBy, format, limit
-module.exports.select = function() {
-    var selectors = [].slice.call(arguments);
-    return function(collection) {
-        return collection.map(function(contact) {
+module.exports.select = function () {
+    var selectors = Array.prototype.slice.call(arguments, 0);
+    return function (collection) {
+        return collection.map(function (contact) {
             var result = {};
             selectors.forEach(selector => {
                 if (selector in contact) {
@@ -42,24 +42,30 @@ module.exports.select = function() {
     };
 };
 
-module.exports.filterIn = function(selector, values) {
-    return function(collection) {
-        return collection.filter(function(contact) {
+module.exports.filterIn = function (selector, values) {
+    return function (collection) {
+        return collection.filter(function (contact) {
             return values.some(value => value === contact[selector]);
         });
     };
 };
 
-module.exports.filterEqual = function(selector, value) {
-    return function(collection) {
+module.exports.filterEqual = function (selector, value) {
+    return function (collection) {
         return collection.filter(contact => value === contact[selector]);
     };
 };
 
-module.exports.sortBy = function(selector, type) {
-    return function(collection) {
-        type = type === 'asc' ? 1 : -1;
-        return collection.sort(function(contact1, contact2) {
+module.exports.sortBy = function (selector, type) {
+    return function (collection) {
+        if (type === 'asc') {
+            type = 1;
+        } else if (type === 'desc') {
+            type = -1;
+        } else {
+            type = 0;
+        }
+        return collection.sort(function (contact1, contact2) {
             if (contact1[selector] > contact2[selector]) {
                 return type;
             } else {
@@ -69,8 +75,8 @@ module.exports.sortBy = function(selector, type) {
     };
 };
 
-module.exports.format = function(selector, f) {
-    return function(collection) {
+module.exports.format = function (selector, f) {
+    return function (collection) {
         collection.forEach(contact => {
             contact[selector] = f(contact[selector]);
         });
