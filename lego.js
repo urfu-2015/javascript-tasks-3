55299810d168204dda40219554e3247c949438d7
AlSoEdit <alexsprint27@mail.ru>
2015-11-23 16:38:58 +0500

    Update lego.js

--- lego.js
+++ lego.js
@@ -18,76 +18,63 @@ module.exports.reverse = function () {
 module.exports.limit = function (n) {
     // Магия
     return function (collection) {
-        return collection.slice(0, n);
+        if (n > 0) {
+            return collection.slice(0, n);
+        }
+        return collection;
     };
 };
 
 module.exports.select = function () {
     var args = [].slice.call(arguments);
-    var resultCollection = [];
     return function (collection) {
-        collection.forEach(function (contact) {
-            var oneContact = {};
-            args.forEach(function (arg) {
-                oneContact[arg] = contact[arg];
+        collection.reduce(function (contacts, contact) {
+            return args.reduce(function (a, b) {
+                return contact[b];
             });
-            resultCollection.push(oneContact);
-        });
-        return resultCollection;
-    };
-};
-
-module.exports.filterIn = function () {
-    var args = [].slice.call(arguments);
-    var resultCollection = [];
-    return function (collection) {
-        collection.forEach(function (contact) {
-            args[1].forEach(function (arg) {
-                if (contact[args[0]] === arg) {
-                    resultCollection.push(contact);
-                }
-            });
-        });
-        return resultCollection;
-    };
-};
-
-module.exports.sortBy = function () {
-    var args = [].slice.call(arguments);
-    return function (collection) {
-        collection.sort(checkResult);
-        if (args[1] == 'asc') {
-            return collection;
-        } else {
-            return collection.reverse();
-        }
-    };
-};
-
-
-function checkResult(firstContact, secondContact) {
-    return firstContact.age - secondContact.age;
-}
-
-module.exports.format = function () {
-    var args = [].slice.call(arguments);
-    return function (collection) {
-        collection.forEach(function (contact) {
-            contact[args[0]] = args[1](contact[args[0]]);
         });
         return collection;
     };
 };
 
-module.exports.filterEqual = function () {
-    var args = [].slice.call(arguments);
+module.exports.filterIn = function (fieldName, fieldValue) {
     var resultCollection = [];
     return function (collection) {
-        collection.forEach(function (contact) {
-            if (contact[args[0]] == args[1]) {
-                resultCollection.push(contact);
-            }
+        return resultCollection = collection.filter(function (contact) {
+            return fieldValue.indexOf(contact[fieldName]) != -1;
         });
-        return resultCollection;
+    };
+};
+
+module.exports.sortBy = function (fieldSort, sortMethod) {
+    return function (collection) {
+        if (sortMethod === 'asc') {
+            return collection.sort(function (a, b) {
+                return a[fieldSort] - b[fieldSort];
+            });
+        } else if (sortMethod === 'desc') {
+            return collection.sort(function (a, b) {
+                return b[fieldSort] - a[fieldSort];
+            });
+        }
+        return collection;
+    };
+};
+
+module.exports.format = function (gender, firstLetter) {
+    return function (collection) {
+        collection.map(function (contact) {
+            contact[gender] = firstLetter(contact[gender]);
+        });
+        return collection;
+    };
+};
+
+module.exports.filterEqual = function (fieldName, fieldValue) {
+    var resultCollection = [];
+    return function (collection) {
+        return resultCollection = collection.filter(function (contact) {
+            return contact[fieldName] === fieldValue;
+        });
     };
 };
