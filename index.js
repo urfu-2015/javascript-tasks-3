0680e47d4dfdadd6a623e6a54f2f0aeba155878e
Natalia Makishvili <natamaki@yandex-team.ru>
2016-02-08 00:40:05 +0300

    Natalia Makishvili

--- index.js
+++ index.js
@@ -29,21 +29,23 @@ var result = lego.query(
     // На дачу влезет примерно 10 человек
     lego.limit(10)
 );
+console.log(result);
+
 
 // Будет круто организовать две вечеринки сразу: яблочную для девушек и картофельную для парней.
 
-var result = lego.query(
-    phoneBook,
-
-    // Выбираем всех парней, которые любят картофель, и всех девушек, которые любят яблоки
-    lego.or(
-        lego.and(
-            lego.filterEqual('gender', 'Мужской'),
-            lego.filterIn('favoriteFruit', ['Картофель'])
-        ),
-        lego.and(
-            lego.filterEqual('gender', 'Женский'),
-            lego.filterIn('favoriteFruit', ['Яблоко'])
-        )
-    )
-);
+//var result = lego.query(
+//    phoneBook,
+//
+//    // Выбираем всех парней, которые любят картофель, и всех девушек, которые любят яблоки
+//    lego.or(
+//        lego.and(
+//            lego.filterEqual('gender', 'Мужской'),
+//            lego.filterIn('favoriteFruit', ['Картофель'])
+//        ),
+//        lego.and(
+//            lego.filterEqual('gender', 'Женский'),
+//            lego.filterIn('favoriteFruit', ['Яблоко'])
+//        )
+//    )
+//);
--- lego.js
+++ lego.js
@@ -1,8 +1,14 @@
 'use strict';
 
 // Метод, который будет выполнять операции над коллекцией один за другим
-module.exports.query = function (collection /* операторы через запятую */) {
+module.exports.query = function () {
+    var collection = arguments[0];
 
+    for (var i = 1; i < arguments.length; i++) {
+        collection = arguments[i](collection);
+    }
+
+    return collection;
 };
 
 // Оператор reverse, который переворачивает коллекцию
@@ -15,13 +21,63 @@ module.exports.reverse = function () {
     };
 };
 
-// Оператор limit, который выбирает первые N записей
-module.exports.limit = function (n) {
-    // Магия
+// Оператор select, который выбирает только нужные поля
+module.exports.select = function () {
+    var args = arguments;
+
+    return function (collection) {
+        return collection.map(function (item) {
+            var changedItem = {};
+
+            for (var i = 0; i < args.length; i++) {
+                var fieldName = args[i];
+                changedItem[fieldName] = item[fieldName];
+            }
+
+            return changedItem;
+        });
+    }
 };
 
-// Вам необходимо реализовать остальные операторы:
-// select, filterIn, filterEqual, sortBy, format, limit
+// Обязательно выбираем тех, кто любит Яблоки и Картофель (самое важное !!!)
+module.exports.filterIn = function (field, type) {
+    return function (collection) {
+        return collection.filter(function(item){
+            return type.indexOf(item[field]) !== -1;
+        });
+    };
+};
+
+// Отсортируем их по возрасту
+module.exports.sortBy = function (field, type) {
+    return function (collection) {
+        return collection.sort(function (a, b) {
+            var result = Number(a[field]) - Number(b[field]);
+            return type === 'desc' ? -result : result;
+        });
+    }
+};
+
+// А пол выведем только первой буквой для удобства
+module.exports.format = function (field, fn) {
+    return function (collection) {
+        return collection.map(function (item) {
+            item[field] = fn(item[field]);
+
+            return item;
+        })
+    };
+};
+
+// Оператор limit, который выбирает первые N записей
+module.exports.limit = function (n) {
+    return function (collection) {
+        return collection.filter(function(item, i){
+            return i < n;
+        });
+    };
+};
 
 // Будет круто, если реализуете операторы:
 // or и and
+//реализую в следующем PR
